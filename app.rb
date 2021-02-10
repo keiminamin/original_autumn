45de91e383dd925c9b9157aa262069b00818f9a1
keiminamin <ktaro0512@docomo.ne.jp>
2021-02-10 10:44:35 +0000

    nine

--- app.rb
+++ app.rb
@@ -206,10 +206,10 @@ post '/group/:id/post' do
   @group = Group.find(params[:id])
     Board.create( {board_title: params[:board_title],board_content: params[:board_content],user_id: current_user.id, group_id:@group.id})
 
-    users = User.all
+    users = @group.users.all
     users.each do |user|
 　
-      userid = user.find_by(id: @group.usergroups.user_id).line_id
+      userid = user.line_id
   message = { type: 'text', text: "グループに依頼が投稿されました。確認してみましょう
     https://shareboards-0512.herokuapp.com/" }
 
