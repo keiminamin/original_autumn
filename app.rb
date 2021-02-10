103e23ec02016f8a3fa6efea33cc60165d1fd5da
keiminamin <ktaro0512@docomo.ne.jp>
2021-02-10 13:20:55 +0000

    eleven

--- app.rb
+++ app.rb
@@ -278,9 +278,12 @@ post '/:id/evaluate' do
   # content.user.evaluation = params[:evaluation]
   content.confirm_date = Date.today
   content.save!
-  userid = User.find_by(id: content.user_id).line_id
-  message = { type: 'text', text: "#{User.find_by(id: content.custome_id).name}さんが確認しました。これで取引完了です
+  userid = User.find_by(id: content.custome_id).line_id
+  message = { type: 'text', text: "#{User.find_by(id: content.user_id).name}さんが確認しました。これで取引完了です
     https://shareboards-0512.herokuapp.com/" }
+
+    client.push_message(userid, message)
+
   redirect '/'
 end
 
